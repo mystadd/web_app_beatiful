086edbd1f9b4a4a9316db2009a6c4ce474843333
dima <di.mystadd@gmail.com>
2024-06-24 22:47:54 +0300

    сообщение

--- js/script.js
+++ js/script.js
@@ -15,11 +15,8 @@ function sendTelegramMessage(selectedSpecialist, selectedServices, selectedDateT
   FullMessage += `<b>Имя:</b> ${name}\n`;
   FullMessage += `<b>Телефон:</b> ${telephone}\n`;
 
-  if (email) {
+  if (email || message) {
     FullMessage += `<b>E-mail:</b> ${email}\n`;
-  }
-                  
-  if (message) {
     FullMessage += `<b>Комментарий:</b> ${message}`;
   }
 
